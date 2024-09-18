b46692b72274385ad2b019c8c7da5fecb1521c9c
artsyl3 <118113234+artStyl3@users.noreply.github.com>
2024-09-18 09:50:49 +0200

    saa

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 const fs = require('fs')
-fs.writeFileSync('note.txt' , 'I love node js')
+//fs.writeFileSync('note.txt' , 'I lodadadddd js')
+fs.appendFile('note.txt', "I add you", (err) => {
+    if (err) {
+      console.error('Failed to append data:', err);
+    } else {
+      console.log('Data successfully appended.');
+    }
+  });
