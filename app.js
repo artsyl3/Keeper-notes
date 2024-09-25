7ef3a3ac5b4756bf92008333b517c8e670db66c4
artsyl3 <118113234+artStyl3@users.noreply.github.com>
2024-09-25 20:30:40 +0200

    k

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
 const chalk = require('chalk')
 const fs = require('fs')
 const notes = require('./notes');
+const yargs = require('yargs');
 
-const Mynotes = notes();
-console.log(Mynotes)
+
 const word = process.argv[2]
+console.log()
 
-if (word==='add') {
-    console.log('Adding a new note...')
-} else if (word==='remove') {
-    console.log('Removing a note...')
-}
 
-console.log(chalk.blue.inverse.bold('Success..'))
 
