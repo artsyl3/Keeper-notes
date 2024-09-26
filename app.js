e0744c103e9e4c9e471af5a7919e25ee8b17f643
artsyl3 <118113234+artStyl3@users.noreply.github.com>
2024-09-26 19:21:16 +0200

    lk

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ yargs.command({
         }
     },
     handler: function (argv) {
-        console.log(argv.title , argv.body)
+        console.log("title: ", chalk.bold(argv.title) , "body: ", chalk.bold(argv.body));
     }
 })
 
