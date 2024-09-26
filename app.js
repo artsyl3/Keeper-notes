c21471acbcfef4e225f47f5a0fa15f90efc798e6
artsyl3 <118113234+artStyl3@users.noreply.github.com>
2024-09-26 09:56:44 +0200

    changes

--- app.js
+++ app.js
@@ -3,9 +3,38 @@ const fs = require('fs')
 const notes = require('./notes');
 const yargs = require('yargs');
 
+yargs.version('1.1.0')
 
-const word = process.argv[2]
-console.log()
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function () {
+        console.log('Note added successfully!')
+    }
+})
 
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Note removed successfully!')
+    }
+})
 
+yargs.command({
+    command: 'list',
+    describe: 'List all notes',
+    handler: function () {
+        console.log('Listing notes...')
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Reading note...')
+    }
+})
+console.log(yargs.argv)
 
