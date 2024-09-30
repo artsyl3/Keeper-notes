c108356667b7d98f6c104a2dadeaa879447ba56e
artsyl3 <118113234+artStyl3@users.noreply.github.com>
2024-09-30 02:03:43 +0200

    dad

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ yargs.command({
         }
     },
     handler: function (argv) {
-        console.log("title: ", chalk.bold(argv.title) , "body: ", chalk.bold(argv.body));
+        notes.addNotes(argv.title, argv.body)
     }
 })
 
--- notes.js
+++ notes.js
@@ -1,5 +1,35 @@
+const fs = require('fs');
+
 const getNotes = function ( ) {
     return ("your notes...")
 }
 
-module.exports = getNotes;
+const addNote = function (title, body) {
+    const notes = loadNotes();
+    notes.push({
+        title: title,
+        body: body
+    })
+
+    saveNotes(notes);
+}
+
+const saveNotes = function (notes) {
+    const JsonNOtes = JSON.stringify(notes);
+    fs.writeFileSync('notes.json', jsonNotes);
+}    
+
+const loadNotes = function ( ) {
+    try {
+    const dataBuffer = fs.readFileSync('notes.json');
+    const data = JSON.parse(dataBuffer.toString());
+    return data;
+    } catch (error) {
+        return [];  
+    }
+}
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote
+}
